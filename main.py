07fa52c99aff7d54af6728a29f8bc7b4131129bb
dmitriy <novyj456@mail.ru>
2021-12-29 22:36:56 +0300

    Initial project code

---
+++ main.py
@@ -0,0 +1,68 @@
+import pygame
+import os
+
+pygame.init()
+size = width, height = 1910, 990
+screen = pygame.display.set_mode(size)
+
+
+def load_image(name, color_key=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname).convert()
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+class Mountain(pygame.sprite.Sprite):
+    image = load_image("theme.png", -1)
+
+    def __init__(self):
+        super().__init__(all_sprites)
+        self.image = Mountain.image
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.bottom = height
+
+
+class Landing(pygame.sprite.Sprite):
+    image = load_image("parach.png", -1)
+
+    def __init__(self, pos):
+        super().__init__(all_sprites)
+        self.image = Landing.image
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.x = pos[0] - 100
+        self.rect.y = pos[1] - 70
+
+    def update(self):
+        if not pygame.sprite.collide_mask(self, mountain):
+            self.rect = self.rect.move(0, 1)
+
+
+all_sprites = pygame.sprite.Group()
+mountain = Mountain()
+running = True
+clock = pygame.time.Clock()
+fps = 60
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            Landing(event.pos)
+    clock.tick(fps)
+    screen.fill((0, 191, 255))
+    all_sprites.draw(screen)
+    all_sprites.update()
+    pygame.display.flip()
